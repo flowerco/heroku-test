bc05ec3b12a3ab7f4a1b862cdddd7c76cfe381cd
Oliver Pinwich <olive@macbook-pro.home>
2022-02-12 03:19:42 +0000

    Additional search for MP image and improved page layout

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import os
-import re
 import datetime
 import requests
-import sys
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, get_mp_name, get_mp_details, get_donations, gbp
 
 # Configure application
 app = Flask(__name__)
@@ -19,31 +15,12 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Custom filter
-app.jinja_env.filters["usd"] = usd
+app.jinja_env.filters["gbp"] = gbp
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-# Configure Flask to use SQLAlchemy database
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-# rest of connection code using the connection string `uri`
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-
-    def __init__(self, name):
-        self.name = name
 
 
 @app.after_request
@@ -57,112 +34,44 @@ def after_request(response):
 
 @app.route("/")
 def index():
-    """Show list of users"""
-
-    users = User.query.order_by(User.id).all()
-
-    if len(users) == 0:
-        return apology("No users")
-
-    return render_template("/index.html", users = users)
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user"""
-
-    if request.method == "POST":
-
-        user = request.form.get("username")
-        user_check = User.query.filter_by(name=user).first()
-
-        # Input validation
-        if not user:
-            return apology("Please enter a username")
-        if user_check:
-            return apology("Username already exists")
-
-        # If all valid, we add the new data to the SQL db and submit to /register.
-        else:
-            # New PostgreSQL implementation 
-            new_user = User(user)
-            db.session.add(new_user)
-            db.session.commit()
-
-            return redirect("/")
-
-    # A method other than POST indicated the user was redirected.
-    # Submitting the form will use a POST method.
-    else:
-        return render_template("/register.html")
-
+    """Show homepage with search bars"""
+    return render_template("/index.html")
 
 # Test page to fetch the donor data
-@app.route("/fetch", methods = ["GET","POST"])
+@app.route("/fetch", methods = ["POST"])
 def fetch():
 
-    if request.method=="POST":
-
-        # Create an array of names to identify the first and surname.
-        # TODO: remove punctuation, etc. Regex would probably be ideal for cleaning the input.
-        name_arr = request.form.get("mpname").split()
-
-        if not name_arr:
+    # If the user is searching by postcode we need an extra step to identify the MP
+    postcode = request.form.get("postcode")
+    # Create an array of names to identify the first and surname.
+    # TODO: remove punctuation, etc. Regex would probably be ideal for cleaning the input.    
+    if postcode:
+        mp_name = get_mp_name(postcode)
+        if not mp_name:
+            return apology("No MP found for that postcode")
+    else:
+        mp_name = request.form.get("mpname")
+        if not mp_name:
             return apology("Invalid name entered")
 
-        mp_first = name_arr[0]
-        mp_last = name_arr[len(name_arr)-1]
+    # Once the MP name is defined, we can get their ID and thumbnail image
+    mp_id, mp_thumb = get_mp_details(mp_name)
 
-        # Identify the current date as the maximum date to query.
-        run_dt = datetime.date.today().strftime("%Y-%m-%d")
-        
-        donor_url = 'http://search.electoralcommission.org.uk/api/search/Donations?' +\
-            '&query=' + mp_first + '%20' + mp_last +\
-                '&sort=AcceptedDate&order=desc&tab=1&open=filter&closed=common&et=pp&et=ppm&et=tp&et=perpar&et=rd&isIrishSourceYes=true&isIrishSourceNo=true&date=Received' +\
-                    '&from=2017-01-01&to=' + run_dt +\
-                        '&prePoll=false&postPoll=true&donorStatus=individual&donorStatus=tradeunion&donorStatus=company&donorStatus=unincorporatedassociation&donorStatus=publicfund&donorStatus=other&donorStatus=registeredpoliticalparty' +\
-                            '&donorStatus=friendlysociety&donorStatus=trust&donorStatus=limitedliabilitypartnership&donorStatus=impermissibledonor&donorStatus=na&donorStatus=unidentifiabledonor&donorStatus=buildingsociety&register=gb' +\
-                                '&register=ni&register=none&optCols=Register&optCols=CampaigningName&optCols=AccountingUnitsAsCentralParty&optCols=IsSponsorship&optCols=IsIrishSource&optCols=RegulatedDoneeType&optCols=CompanyRegistrationNumber' +\
-                                    '&optCols=Postcode&optCols=NatureOfDonation&optCols=PurposeOfVisit&optCols=DonationAction&optCols=ReportedDate&optCols=IsReportedPrePoll&optCols=ReportingPeriodName&optCols=IsBequest&optCols=IsAggregation'
+    if not mp_id:
+        return apology(mp_name + " is not a current MP")
 
-        # donor_url = 'http://search.electoralcommission.org.uk/api/search/Donations?&query=Hilary%20Benn&sort=AcceptedDate&order=desc&tab=1&open=filter&closed=common&et=pp&et=ppm&et=tp&et=perpar&et=rd&isIrishSourceYes=true&isIrishSourceNo=true&date=Received&from=2017-01-01&to=2022-02-28&prePoll=false&postPoll=true&donorStatus=individual&donorStatus=tradeunion&donorStatus=company&donorStatus=unincorporatedassociation&donorStatus=publicfund&donorStatus=other&donorStatus=registeredpoliticalparty&donorStatus=friendlysociety&donorStatus=trust&donorStatus=limitedliabilitypartnership&donorStatus=impermissibledonor&donorStatus=na&donorStatus=unidentifiabledonor&donorStatus=buildingsociety&register=gb&register=ni&register=none&optCols=Register&optCols=CampaigningName&optCols=AccountingUnitsAsCentralParty&optCols=IsSponsorship&optCols=IsIrishSource&optCols=RegulatedDoneeType&optCols=CompanyRegistrationNumber&optCols=Postcode&optCols=NatureOfDonation&optCols=PurposeOfVisit&optCols=DonationAction&optCols=ReportedDate&optCols=IsReportedPrePoll&optCols=ReportingPeriodName&optCols=IsBequest&optCols=IsAggregation'
+    # Now that we have all required details for the MP, we can search for their donors
+    my_donors = get_donations(mp_name)
 
-        # Request the URL and parse the JSON
-        response = requests.get(donor_url)
-        response.raise_for_status() # raise exception if invalid response
-        all_donors = response.json()['Result']
+    mp = {'name':mp_name, 'thumbnail': mp_thumb}
 
-        if not all_donors:
-            return apology("No Donors Found")
-
-        # Specify the attributes we want to keep
-        donor_cols = [
-            'AcceptedDate', 'ReturnedDate', 'AttemptedConcealment',
-            'CashValue', 'NonCashValue', 'IsAnonymous', 'RegulatedEntityId', 'RegulatedEntityName',
-            'Value', 'DonorName', 'DonorStatus','RegulatedDoneeType'
-        ]
-
-        # For each row extracted from the JSON, add only the required attributes to a smaller list of dictionaries
-        my_donors = []
-        for row in all_donors:
-            # Check for the selected MP name when adding rows to the output
-            if mp_first in row['RegulatedEntityName'] and mp_last in row['RegulatedEntityName']:
-                temp_dict = {}
-                for col in donor_cols:
-                    temp_dict[col] = row[col]
-                my_donors.append(temp_dict)
-
-        # Supply this subset of the returned JSON to the html page
-        if my_donors:
-            return render_template("/donors.html", donors = my_donors)
-        else:
-            return apology("No Donations Found")
-
-    else:
-        return render_template("/fetch.html")
+    # Supply this subset of the returned JSON to the html page
+    return render_template("/donors.html", mp = mp, donors = my_donors)
 
 
-@app.route("/test")
-def test():
-
-    return render_template("/test.html")
+@app.route('/redirect_to')
+def redirect_to():
+    link = 'http://google.com/search?q='
+    name = request.args.get('link', 'Boris Johnson')
+    new_link = link + name.replace(' ','+')
+    return redirect(new_link), 301
--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 import os
 import requests
-import urllib.parse
+import datetime
 
 from flask import redirect, render_template, request, session
 from functools import wraps
@@ -21,44 +21,91 @@ def apology(message, code=400):
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
-def login_required(f):
-    """
-    Decorate routes to require login.
+def get_mp_name(postcode):
+    """ Search for the name of the MP for a given postcode. """
+    # If the postcode contains spaces, replace with '%20' for the search URL
+    search_string = postcode.replace(" ", "%20")
 
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
+    post_url = 'https://members-api.parliament.uk/api/Location/Constituency/Search?' +\
+        'searchText=' + search_string + '&skip=0&take=20'
 
+    # Request the URL and parse the JSON for the returned constituency
+    constit = requests.get(post_url)
+    constit.raise_for_status() # raise exception if invalid response
+    details = constit.json()['items']
+    if details:
+        mp_data = details[0]['value']['currentRepresentation']['member']['value']
+        mp_name = mp_data['nameDisplayAs']
+    else:
+        mp_name = None
 
-def lookup(symbol):
-    """Look up quote for symbol."""
+    return mp_name
 
-    # Contact API
-    try:
-        api_key = os.environ.get("API_KEY")
-        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException:
+def get_mp_details(name):
+    """ Search for the ID and thumbnail image of the MP by name. """
+    # If the postcode contains spaces, replace with '%20' for the search URL
+    search_string = name.replace(" ", "%20")
+
+    post_url = 'https://members-api.parliament.uk/api/Members/' +\
+        'Search?Name=' + search_string + '&skip=0&take=20'
+
+    # Request the URL and parse the JSON for the returned constituency
+    details = requests.get(post_url)
+    details.raise_for_status() # raise exception if invalid response
+    mp_data = details.json()['items']
+
+    if mp_data:
+        mp_id = mp_data[0]['value']['id']
+        mp_thumb = mp_data[0]['value']['thumbnailUrl']
+    else:
+        mp_id = None
+        mp_thumb = None
+
+    return mp_id, mp_thumb
+
+def get_donations(mp_name):
+
+    name_string = mp_name.replace(" ", "%20")
+
+    # Identify the current date as the maximum date to query.
+    run_dt = datetime.date.today().strftime("%Y-%m-%d")
+        
+    donor_url = 'http://search.electoralcommission.org.uk/api/search/Donations?' +\
+        '&query=' + name_string +\
+            '&sort=AcceptedDate&order=desc&tab=1&open=filter&closed=common&et=pp&et=ppm&et=tp&et=perpar&et=rd&isIrishSourceYes=true&isIrishSourceNo=true&date=Received' +\
+                '&from=2017-01-01&to=' + run_dt +\
+                    '&prePoll=false&postPoll=true&donorStatus=individual&donorStatus=tradeunion&donorStatus=company&donorStatus=unincorporatedassociation&donorStatus=publicfund&donorStatus=other&donorStatus=registeredpoliticalparty' +\
+                        '&donorStatus=friendlysociety&donorStatus=trust&donorStatus=limitedliabilitypartnership&donorStatus=impermissibledonor&donorStatus=na&donorStatus=unidentifiabledonor&donorStatus=buildingsociety&register=gb' +\
+                            '&register=ni&register=none&optCols=Register&optCols=CampaigningName&optCols=AccountingUnitsAsCentralParty&optCols=IsSponsorship&optCols=IsIrishSource&optCols=RegulatedDoneeType&optCols=CompanyRegistrationNumber' +\
+                                '&optCols=Postcode&optCols=NatureOfDonation&optCols=PurposeOfVisit&optCols=DonationAction&optCols=ReportedDate&optCols=IsReportedPrePoll&optCols=ReportingPeriodName&optCols=IsBequest&optCols=IsAggregation'
+
+    # Request the URL and parse the JSON
+    response = requests.get(donor_url)
+    response.raise_for_status() # raise exception if invalid response
+    all_donors = response.json()['Result']
+
+    if not all_donors:
         return None
 
-    # Parse response
-    try:
-        quote = response.json()
-        return {
-            "name": quote["companyName"],
-            "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"]
-        }
-    except (KeyError, TypeError, ValueError):
-        return None
+    # Specify the attributes we want to keep
+    donor_cols = [
+        'AcceptedDate', 'ReturnedDate', 'AttemptedConcealment', 'CashValue', 'NonCashValue',
+        'IsAnonymous', 'Value', 'DonorName', 'DonorStatus'
+    ]
+
+    # For each row extracted from the JSON, add only the required attributes to a smaller list of dictionaries
+    my_donors = []
+    for row in all_donors:
+        # Check for the selected MP name when adding rows to the output
+        if mp_name in row['RegulatedEntityName']:
+            temp_dict = {}
+            for col in donor_cols:
+                temp_dict[col] = row[col]
+            my_donors.append(temp_dict)
+    
+    return my_donors
 
 
-def usd(value):
-    """Format value as USD."""
-    return f"${value:,.2f}"
+def gbp(value):
+    """Format value as GBP."""
+    return f"£{value:,.2f}"
