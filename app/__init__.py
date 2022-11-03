8e0704853ec4da1e2ec2322d691533f55bf90957
Sam Flower <66141971+flowerco@users.noreply.github.com>
2022-11-03 00:41:59 +0000

    Fixed: automatic ssl redirection with flask-talisman

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,25 @@ import os
 from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
 from helpers import gbp
 
 # Configure application
 app = Flask(__name__)
+
+# Trigger SSLify via Talisman if the app is running on Heroku
+if 'DYNO' in os.environ: 
+
+    csp = {
+        'default-src': [
+            '\'self\'',
+            '\'unsafe-inline\'',
+            'code.jquery.com',
+            'cdn.jsdelivr.net'
+        ]
+    }
+    Talisman(app, content_security_policy=csp)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # uri = 'postgresql://postgres@localhost/olive'
 uri = os.getenv("DATABASE_URL")
